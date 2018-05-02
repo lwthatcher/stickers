7887aa128bca874867c1238f3a3b09e9a742c87b
Lawrence Thatcher <lwthatcher@msn.com>
2018-05-01 18:11:59 -0600

    added d3

--- src/app/databar/databar.component.ts
+++ src/app/databar/databar.component.ts
@@ -1,20 +1,32 @@
 import { Component, OnInit } from '@angular/core';
+import * as d3 from "d3";
 
 @Component({
   selector: 'app-databar',
   template: `
-    <svg width="960" height="600"></svg>
+    <svg width="100%" height="600" class="databar" (click)="clicked($event)"></svg>
   `,
-  styles: []
+  styleUrls: ['./databar.component.css']
 })
 export class DatabarComponent implements OnInit {
   width = 960;
-  height = 600;
+  height = 400;
+  radius = 10;
 
   constructor() { }
 
   ngOnInit() {
     console.log('init databar', this);
+  }
+
+  clicked(event: any) {
+    console.log('clicked!', event)
+    d3.select(event.target)
+      .append('circle')
+      .attr('cx', event.offsetX)
+      .attr('cy', event.offsetY)
+      .attr('r', this.radius)
+      .attr('fill', 'red');
   }
 
 }
