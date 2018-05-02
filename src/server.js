798a1c0cba8b4cc58362a472e2280350fb074d51
Lawrence Thatcher <lwthatcher@msn.com>
2018-05-02 15:56:32 -0600

    data route returns parsed numpy file

--- src/server.js
+++ src/server.js
@@ -2,19 +2,24 @@
 const express = require('express');
 const np = require('tfjs-npy');
 const fs = require('fs');
+const path = require('path');
 const app = express();
 
-app.route('/api/data').get((req, res) => {
-    let path = __dirname + '/assets/a.npy';
-    res.send(path);
+function bufferToArrayBuffer(b) {
+    return b.buffer.slice(b.byteOffset, b.byteOffset + b.byteLength);
+}
+
+app.route('/api/data').get(async (req, res) => {
+    let _path = path.join(__dirname, 'assets', 'a.npy');
+    const b = fs.readFileSync(_path, null);
+    const ab = bufferToArrayBuffer(b);
+    res.send(await np.parse(ab));
 });
 
 app.route('/api/cats').get((req, res) => {
-    res.send({
-      cats: [{ name: 'lilly' }, { name: 'lucy' }]
-    });
-  });
+    res.send({cats: [{ name: 'lilly' }, { name: 'lucy' }]});
+});
 
 app.listen(3000, () => {
     console.log('Node server listening on port: 3000');
-  });
+});
