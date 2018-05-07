ba8b71e512558588d93cc3e9cf401e5fde646b59
Lawrence Thatcher <lwthatcher@msn.com>
2018-05-07 17:04:34 -0600

    extracted draw() method

--- src/app/databar/databar.component.ts
+++ src/app/databar/databar.component.ts
@@ -1,8 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef } from '@angular/core';
 import { DataloaderService } from '../dataloader.service';
 import { parse } from "tfjs-npy";
 import * as tf from "@tensorflow/tfjs-core";
 import * as d3 from "d3";
+import { TypedArray } from '@tensorflow/tfjs-core/dist/kernels/webgl/tex_util';
 
 @Component({
   selector: 'app-databar',
@@ -26,44 +27,48 @@ export class DatabarComponent implements OnInit {
   x; y; line;
   // color map
   colors;
+  // data references
+  _tensors: Array<tf.Tensor>;
+  _data: Promise<(Float32Array | Int32Array | Uint8Array)[]>;
 
-  _data: Array<tf.Tensor>;
-
-  constructor(private dataloader: DataloaderService) { }
+  constructor(private el: ElementRef, private dataloader: DataloaderService) { }
 
   ngOnInit() {
     // load data
-    let DATA = this.initData();
+    this._data = this.loadData();
     // selectors
     this.svg = d3.select("svg");
     this.g = d3.select("svg > g.transform")
         .attr("transform", "translate(" + this.margin.left + "," + this.margin.top + ")");
     this.g_sigs = d3.select("g > g.signals");
     // width/height
+    const WIDTH = this.el.nativeElement.offsetWidth;
+    console.log('element width/height:', WIDTH, this.svg.attr("height"));
     this.width = +this.svg.attr("width") - this.margin.left - this.margin.right;
     this.height = +this.svg.attr("height") - this.margin.top - this.margin.bottom;
-    console.log('widht/height', this.width, this.height);
+    console.log('width/height', this.width, this.height);
     // color map
     this.colors = d3.scaleOrdinal(d3.schemeAccent);
-    // line drawing funcs
+    // draw data (when it loads)
+    this.draw(this._data);
+    // log when finished
+    console.log('init databar', this);
+  }
+
+  draw(data) {
+    // set the x/y scales
     this.x = d3.scaleLinear().rangeRound([0, this.width]);
     this.y = d3.scaleLinear().rangeRound([this.height, 0]);
     this.line = d3.line()
-                  .x((d,i) => {return this.x(i)})
-                  .y((d,i) => {return this.y(d)})
-    // when data loads:
-    DATA.then((axes_data) => axes_data.map((axis) => axis.dataSync()) )
-        .then((axes) => this.set_domains(axes))
+                  .x((d,i) => this.x(i))
+                  .y((d,i) => this.y(d));
+    // plot data when ready
+    data.then((axes) => this.set_domains(axes))
         .then((axes) => {
           for (let j = 0; j < axes.length; j++) {
             this.plot_axis(axes[j], j);
           }
     });
-    console.log('init databar', this);
-  }
-
-  clicked(event: any) {
-    console.log('clicked!', event)
   }
 
   plot_axis(data, j) {
@@ -75,6 +80,7 @@ export class DatabarComponent implements OnInit {
               .attr("class", "line line-" + j.toString())
               .attr("stroke", this.colors(j))
               .attr("stroke-width", 1.5)
+              .attr("stroke-opacity", 0.7)
               .attr("d", this.line);
   }
 
@@ -86,10 +92,17 @@ export class DatabarComponent implements OnInit {
     return axes;
   }
 
-  initData() {
+  loadData(): Promise<(Float32Array | Int32Array | Uint8Array)[]> {
     return this.dataloader.getData([0,1,2])
-        .then(t => this._data = t)
-        .then(() => { console.log('loaded data', this._data) })
-        .then(() => { return this._data; })
+        .then(t => this._tensors = t)
+        .then(() => { console.log('loaded tensors', this._tensors); return this._tensors })
+        .then((axes_data) => { return axes_data.map((axis) => axis.dataSync()) })
+  }
+
+
+  clicked(event: any) {
+    console.log('clicked!', event);
+    console.log('svg', this.el.nativeElement, this.el);
+    console.log('svg', this.el.nativeElement.offsetWidth, this.el.nativeElement.offsetHeight);
   }
 }
