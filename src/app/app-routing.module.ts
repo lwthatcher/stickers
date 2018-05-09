e5b465bddc5c2ad78bc1bc1e86660ef6b790d08d
Lawrence Thatcher <lwthatcher@msn.com>
2018-05-09 17:15:05 -0600

    added simple display route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule }             from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DataviewComponent } from './dataview/dataview.component';
+
+const routes: Routes = [
+  { path: 'display', component: DataviewComponent}
+]
 
 @NgModule({
+  imports: [ RouterModule.forRoot(routes) ],
   exports: [ RouterModule ]
 })
 export class AppRoutingModule { }
