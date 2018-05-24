4149b9c0a5deae82009dd23e8abf1084a6367748
Lawrence Thatcher <lwthatcher@msn.com>
2018-05-24 12:37:08 -0600

    if the 'workspace' param is present, resolver filters list down to specified workspace

--- src/app/data-loader/workspace-resolver.service.ts
+++ src/app/data-loader/workspace-resolver.service.ts
@@ -7,19 +7,40 @@ import { WorkspaceLoaderService } from './workspace-loader.service';
 
 @Injectable()
 export class WorkspaceResolver implements Resolve<WorkspaceInfo[]> {
-  _workspaces: Observable<Object[]>;
+  _workspaces: Observable<WorkspaceInfo[]>;
 
   constructor(private loader: WorkspaceLoaderService) { }
 
+  // #region [Accessors]
   get workspaces() {
-    if (!this._workspaces)
-      this._workspaces = this.loader.listWorkspaces();
+    if (!this._workspaces) {
+      let ws = this.loader.listWorkspaces();
+      this._workspaces = ws.pipe(map(this.convert))
+    }
+    else { console.debug('loading memoized workspace list', this._workspaces); }
     return this._workspaces;
   }
+  // #endregion
 
+  // #region [Implementation]
   resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<WorkspaceInfo[]> {
-    let ws = this.workspaces;
-    let convert = (ws: Object[]) => { return ws.map((workspace) => {return new WorkspaceInfo(workspace)}) }
-    return ws.pipe(map(convert));
+    console.debug('RESOLVING WORKSPACE(S)', route, state);
+    if ('workspace' in route.params) {
+      let finder = this.find(route.params.workspace);
+      return this.workspaces.pipe(map(finder));
+    }
+    return this.workspaces;
   }
+  // #endregion
+
+  // #region [Helper Methods]
+  private convert(ws: Object[]): WorkspaceInfo[] {
+    return ws.map((workspace) => {return new WorkspaceInfo(workspace)})
+  }
+
+  private find(name) {
+    let checkname = (workspace) => { return workspace.name === name; }
+    return (ws: WorkspaceInfo[]) => {return ws.filter(checkname)}
+  }
+  // #endregion
 }
