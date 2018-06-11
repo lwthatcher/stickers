faffe85b585c0ecb0a2d713da52b1496c20349f6
Lawrence Thatcher <lwthatcher@msn.com>
2018-06-11 15:40:58 -0600

    delete unused KeysPipe

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,6 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
-import { KeysPipe } from './keys.pipe';
 import { AppRoutingModule } from './app-routing.module';
 import { SettingsModule } from './settings/settings.module';
 import { DataLoaderModule } from './data-loader/data-loader.module';
@@ -16,8 +15,7 @@ import { DataviewModule } from './dataview/dataview.module';
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent,
-    KeysPipe
+    HomeComponent
   ],
   imports: [
     BrowserModule,
