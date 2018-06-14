1fa42d089daa526d5a3bf9683d1207a8fdda9e25
Lawrence Thatcher <lwthatcher@msn.com>
2018-06-14 15:28:11 -0600

    extracted modes toolbox template

--- src/app/dataview/modes-toolbox/modes-toolbox.component.ts
+++ src/app/dataview/modes-toolbox/modes-toolbox.component.ts
@@ -1,19 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { ToolMode } from '../databar/tool-mode.enum';
+import { Sensor } from '../sensor';
 
 @Component({
-  selector: 'app-modes-toolbox',
-  template: `
-    <p>
-      modes-toolbox works!
-    </p>
-  `,
+  selector: 'toolbox-modes',
+  templateUrl: 'modes-toolbox.component.html',
   styles: []
 })
 export class ModesToolboxComponent implements OnInit {
 
-  constructor() { }
+  // #region [Variables]
+  mode: ToolMode;
+  TOOL_MODE = ToolMode;
+  // #endregion
 
-  ngOnInit() {
+  // #region [Inputs]
+  @Input() sensor: Sensor;
+  // #endregion
+
+  // #region [Constructors]
+  constructor() {
+    // have some random initial value
+    this.mode = ToolMode.Selection;
   }
 
+  ngOnInit() {
+    console.log('modes-toolbox init!', this);
+  }
+  // #endregion
+
+  // #region [Event Handlers]
+  changed(event) {
+    console.log('mode change:', event);
+  }
+  // #endregion
 }
