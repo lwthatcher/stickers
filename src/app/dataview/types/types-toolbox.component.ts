e15bf5c2dc8c129745163d5a09a4f736ef8af2dd
Lawrence Thatcher <lwthatcher@msn.com>
2018-06-15 12:23:40 -0600

    added types-toobox stub

---
+++ src/app/dataview/types/types-toolbox.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-types-toolbox',
+  template: `
+    <p>
+      types-toolbox works!
+    </p>
+  `,
+  styles: []
+})
+export class TypesToolboxComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
