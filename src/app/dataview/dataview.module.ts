dce70e0f02b45ca19cd80651dd799c52c7bfa689
Lawrence Thatcher <lwthatcher@msn.com>
2018-06-20 17:19:24 -0600

    created stub for sensors-toolbox component

--- src/app/dataview/dataview.module.ts
+++ src/app/dataview/dataview.module.ts
@@ -7,6 +7,7 @@ import { FormsModule } from '@angular/forms';
 import { ModesToolboxComponent } from './modes/modes-toolbox.component';
 import { TypesToolboxComponent } from './types/types-toolbox.component';
 import { LabelstreamToolboxComponent } from './labelstreams/labelstreams-toolbox.component';
+import { SensorsToolboxComponent } from './sensors/sensors-toolbox.component';
 
 @NgModule({
   imports: [
@@ -14,7 +15,13 @@ import { LabelstreamToolboxComponent } from './labelstreams/labelstreams-toolbox
     NgbModule,
     FormsModule
   ],
-  declarations: [DataviewComponent, DatabarComponent, ModesToolboxComponent, TypesToolboxComponent, LabelstreamToolboxComponent],
+  declarations: [
+    DataviewComponent, 
+    DatabarComponent, 
+    ModesToolboxComponent, 
+    TypesToolboxComponent, 
+    LabelstreamToolboxComponent, 
+    SensorsToolboxComponent],
   providers: []
 })
 export class DataviewModule { }
---
+++ src/app/dataview/sensors/sensors-toolbox.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-sensors-toolbox',
+  templateUrl: './sensors-toolbox.component.html',
+  styleUrls: ['./sensors-toolbox.component.css']
+})
+export class SensorsToolboxComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
