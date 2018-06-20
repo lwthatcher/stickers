b661beba48afeffe984d276550c708ddd6504021
Lawrence Thatcher <lwthatcher@msn.com>
2018-06-20 11:04:38 -0600

    added template html to labelstreams-toolbox

--- src/app/dataview/labelstreams/labelstreams-toolbox.component.ts
+++ src/app/dataview/labelstreams/labelstreams-toolbox.component.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Sensor } from '../sensor';
+import { LabelStream } from './labelstream';
+
+// #region [Interfaces]
+type LabelStreamMap = { [name: string]: LabelStream }
+// #endregion
 
 @Component({
   selector: 'toolbox-labelstreams',
@@ -6,10 +12,33 @@ import { Component, OnInit } from '@angular/core';
   styles: []
 })
 export class LabelstreamToolboxComponent implements OnInit {
+  // #region [Variables]
 
+  // #endregion
+
+  // #region [Inputs]
+  @Input() sensor: Sensor;
+  @Input() labelstreams: LabelStreamMap;
+  // #endregion
+
+  // #region [Constructors]
   constructor() { }
 
   ngOnInit() {
   }
+  // #endregion
 
+  // #region [Accessors]
+  get streams(): string[] { return Object.keys(this.labelstreams) }
+  // #endregion
+
+  // #region [Public Methods]
+  toggleLabels(sensor: Sensor) {
+
+  }
+
+  selectStream(sensor: Sensor, stream: string) {
+
+  }
+  // #endregion
 }
