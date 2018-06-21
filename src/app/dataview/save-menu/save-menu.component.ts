fca2e67a0d57c676fbe2969ebdf8c8b468ef1903
Lawrence Thatcher <lwthatcher@msn.com>
2018-06-21 11:02:21 -0600

    extracted save-menu template

--- src/app/dataview/save-menu/save-menu.component.ts
+++ src/app/dataview/save-menu/save-menu.component.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { LabelStream } from '../labelstreams/labelstream';
+
+// #region [Interfaces]
+type LabelStreamMap = { [name: string]: LabelStream }
+// #endregion
+
 
 @Component({
   selector: 'app-save-menu',
@@ -6,10 +12,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./save-menu.component.css']
 })
 export class SaveMenuComponent implements OnInit {
+  // #region [Inputs]
+  @Input() labelstreams: LabelStreamMap
+  // #endregion
 
+  // #region [Constructors]
   constructor() { }
 
   ngOnInit() {
+    console.groupCollapsed('save-menu');
+    console.info('save-menu initialized', this);
+    console.groupEnd();
+  }
+  // #endregion
+
+  // #region [Accessors]
+  get streams(): string[] { return Object.keys(this.labelstreams) }
+  // #endregion
+
+  // #region [Public Methods]
+  save_labels() {
+    console.log('save labels');
   }
 
+  change_ls(stream: string) {
+    console.log('selecting stream to save:', stream);
+  }
+  // #endregion
 }
