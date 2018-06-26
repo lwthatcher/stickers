981657a35cd29f032ce0e96c896e6b1ecf960bf0
Lawrence Thatcher <lwthatcher@msn.com>
2018-06-26 17:18:36 -0600

    x-axis now displayed in minutes/seconds

--- src/app/dataview/databar/drawer.ts
+++ src/app/dataview/databar/drawer.ts
@@ -237,7 +237,7 @@ export class Drawer {
         .attr('class', 'x-axis')
         .attr('transform', 'translate(0,' + this.databar.height + ')')
         .call(d3.axisBottom(this.x)
-                .tickFormat((d) => { return d / 1000; }));
+                .tickFormat((d) => { return time_format(d) }));
   }
 
   draw_yAxis() {
@@ -574,4 +574,15 @@ export class Drawer {
     console.groupEnd();
   }
   // #endregion
+}
+
+function time_format(ms: number) {
+  let secs = Math.floor(ms % (1000 * 60) / 1000);
+  let mins = Math.floor(ms % (1000 * 60 * 60) / (1000 * 60));
+  let hours = Math.floor(ms / (1000 * 60 * 60));
+  let result = ""
+  if (hours > 0) result += hours.toString() + ":" + mins.toString().padStart(2,'0');
+  else result += mins.toString();
+  result += ':' + secs.toString().padStart(2, '0');
+  return result;
 }
