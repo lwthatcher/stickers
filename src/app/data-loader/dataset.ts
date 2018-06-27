eb12829dc1e077bf4a596fc7e58c1d2ca1644b0e
Lawrence Thatcher <lwthatcher@msn.com>
2018-06-27 17:30:25 -0600

    internal Dataset class cleanup

--- src/app/data-loader/dataset.ts
+++ src/app/data-loader/dataset.ts
@@ -26,28 +26,23 @@ type SignalStream = (Float32Array | Int32Array | Uint8Array | number[])[]
   // #endregion
 
 export abstract class Dataset {
-    // #region [Properties]
+    // #region [Constructor]
     axes: Axes;
     info: DataInfo;
-    // #endregion
-    
-    // #region [Constructor]
     constructor(info: DataInfo) { this.info = info }
     // #endregion
     
-    // #region [Abstract Methods]
-    abstract filter(sensor: SensorLike): Axes;
-    abstract format(axes): SignalStream;
-    // #endregion
-    
     // #region [Public Methods]
-    get(sensor: SensorLike) {
-        return this.format(this.filter(sensor)).map((axis) => this.toArray(axis))
-    }
+    get(sensor: SensorLike) { return this.format(this.filter(sensor)).map((axis) => this.toArray(axis)) }
     // #endregion
 
-    // #region [Protected Methods]
+    // #region [Helper Methods]
+    protected abstract format(axes): SignalStream;
+
     protected toArray(axis): datum[] { return Array.from(axis).map((d,i) => this.convert(d,i)) }
+
+    protected filter(sensor: SensorLike): Axes { return this.axes.filter((e,i) => sensor.idxs.includes(i)) }
+
     protected convert(d, _i) {
         let i = (_i * this.info.rate );
         return {d, i};
@@ -55,46 +50,26 @@ export abstract class Dataset {
     // #endregion
 }
 
+// #region [Helper Classes]
 export class TensorDataset extends Dataset {
-    // #region [Constructor]
     axes: Array<tf.Tensor>;
     constructor(axes: Array<tf.Tensor>, info: DataInfo) {
         super(info);
         this.axes = axes;
     }
-    // #endregion
-
-    // #region [Implementation]
-    format(axes: tf.Tensor[]) { return axes.map((axis) => axis.dataSync()) }
-
-    filter(sensor: SensorLike): Axes {
-        let idx = sensor.idxs;
-        const newaxes = this.axes.filter((e,i) => idx.includes(i));
-        return newaxes;
-    }
-    // #endregion
+    protected format(axes: tf.Tensor[]) { return axes.map((axis) => axis.dataSync()) }
 }
   
 export class CSVDataset extends Dataset {
-    // #region [Constructor]
     axes: number[][];
     constructor(axes: number[][], info: DataInfo, transpose = true) {
         super(info);
         if (transpose) this.axes = math.transpose(axes); 
         else this.axes = axes;
     }
-    // #endregion
-
-    // #region [Implementation]
-    format(axes): SignalStream { return axes as SignalStream }
-
-    filter(sensor: SensorLike): Axes {
-        let idx = sensor.idxs;
-        const newaxes = this.axes.filter((e,i) => idx.includes(i));
-        return newaxes;
-    }
-    // #endregion
+    protected format(axes): SignalStream { return axes as SignalStream }
 }
+// #endregion
 
 export class BDLDataset extends Dataset {
     // #region [Constructor]
@@ -104,10 +79,6 @@ export class BDLDataset extends Dataset {
     // #endregion
 
     // #region [Implementation]
-    filter(sensor: SensorLike): Axes {
-        throw new Error("Method not implemented.");
-    }
-
     format(): (Float32Array | Int32Array | Uint8Array | number[])[] {
         throw new Error("Method not implemented.");
     }
