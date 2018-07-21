f5c149c0847916fe92ab8eb2dced341b882e9b41
Lawrence Thatcher <lwthatcher@msn.com>
2018-07-21 14:29:05 -0600

    updated specs to pass tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,19 +1,41 @@
 import { TestBed, async } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { HomeComponent } from './home/home.component';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatIconModule } from '@angular/material';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { SettingsModule } from './settings/settings.module';
+import { DataLoaderModule } from './data-loader/data-loader.module';
+import { DataviewModule } from './dataview/dataview.module';
+import {APP_BASE_HREF} from '@angular/common';
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
-        AppComponent
+        AppComponent,
+        HomeComponent
       ],
+      imports: [BrowserModule,
+        HttpClientModule,
+        AppRoutingModule,
+        BrowserAnimationsModule,
+        MatIconModule,
+        NgbModule.forRoot(),
+        SettingsModule,
+        DataLoaderModule,
+        DataviewModule],
+      providers: [{provide: APP_BASE_HREF, useValue: '/my/app'}]
     }).compileComponents();
   }));
-  it('should create the app', async(() => {
+  it('creates app', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   }));
-  it(`should have as title 'Stickers'`, async(() => {
+  it(`title = 'Stickers'`, async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app.title).toEqual('Stickers');
