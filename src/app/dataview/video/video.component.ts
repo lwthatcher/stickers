bda45677a497757c584ef727a23ff5fa444dc97a
Lawrence Thatcher <lwthatcher@msn.com>
2018-07-21 15:37:05 -0600

    example video stub

--- src/app/dataview/video/video.component.ts
+++ src/app/dataview/video/video.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, ElementRef } from '@angular/core';
+import { WorkspaceInfo } from '../../data-loader/workspace-info';
+import { VgAPI } from 'videogular2/core';
 
 @Component({
   selector: 'app-video',
@@ -6,10 +8,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./video.component.scss']
 })
 export class VideoComponent implements OnInit {
+  // #region [Properties]
+  preload: string = 'auto';
+  api: VgAPI;
+  video: HTMLVideoElement;
+  // #endregion
 
-  constructor() { }
+  // #region [Inputs]
+  @Input() workspace: WorkspaceInfo;
+  // #endregion
+
+  // #region [Constructors]
+  constructor(private el: ElementRef) { }
 
   ngOnInit() {
+    console.groupCollapsed('video component');
+    this.video = this.el.nativeElement.querySelector('vg-player > video');
+    console.log('video element:', this.video);
+    console.info('video component init', this);
+    console.groupEnd();
   }
+  // #endregion
 
+  // #region [Event Handlers]
+  onPlayerReady(api: VgAPI) {
+    console.log('video player ready', this.api);
+  }
+  // #endregion
 }
