2e9becbde83e3336bb30404631b8d646e3427dcb
Lawrence Thatcher <lwthatcher@msn.com>
2018-07-26 15:41:20 -0600

    added additional unit tests

--- src/app/util/sync.spec.ts
+++ src/app/util/sync.spec.ts
@@ -6,39 +6,51 @@ describe('Synchronizer', () => {
     
     beforeEach(() => { });
    
-    it('canSync = false -- data empty', () => {
+    it('canSync = false -- (data empty)', () => {
         let [dt, vt] = [[], [4.28888]]
         let sync = new Synchronizer(dt, vt);
         expect(sync.canSync).toBe(false);
     });
 
-    it('canSync = false -- video empty', () => {
+    it('canSync = false -- (video empty)', () => {
         let [dt, vt] = [[0, 669470], []]
         let sync = new Synchronizer(dt, vt);
         expect(sync.canSync).toBe(false);
     });
 
-    it('canSync = false -- both empty', () => {
+    it('canSync = false -- (both empty)', () => {
         let [dt, vt] = [[], []]
         let sync = new Synchronizer(dt, vt);
         expect(sync.canSync).toBe(false);
     });
 
-    it('canSync = false -- mismatch', () => {
+    it('canSync = false -- (mismatch)', () => {
         let [dt, vt] = [[null, 1000], [1]]
         let sync = new Synchronizer(dt, vt);
         expect(sync.canSync).toBe(false);
     });
 
-    it('canSync = true -- same size', () => {
+    it('canSync = true -- (same size)', () => {
         let [dt, vt] = [[0], [4.28888]]
         let sync = new Synchronizer(dt, vt);
         expect(sync.canSync).toBe(true);
     });
 
-    it('canSync = false -- different sizes', () => {
+    it('canSync = false -- (different sizes)', () => {
         let [dt, vt] = [[0, 669470], [4.28888]]
         let sync = new Synchronizer(dt, vt);
         expect(sync.canSync).toBe(true);
     });
+
+    it('video -> data', () => {
+        let [dt, vt] = [[0, 623390], [11.89045]]
+        let sync = new Synchronizer(dt, vt);
+        expect(sync.vidToData(11.89045)).toBeCloseTo(0);
+    })
+
+    it('data -> video', () => {
+        let [dt, vt] = [[0, 623390], [11.89045]]
+        let sync = new Synchronizer(dt, vt);
+        expect(sync.dataToVid(0)).toBeCloseTo(11.89045);
+    })
 });
