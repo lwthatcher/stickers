f7003876fd0ffda565e635f1296a35fc5fd63211
Lawrence Thatcher <lwthatcher@msn.com>
2018-07-26 11:22:38 -0600

    sorts default data/scheme, updated data/scheme badges

--- src/app/home/workspace/workspace.component.ts
+++ src/app/home/workspace/workspace.component.ts
@@ -36,8 +36,8 @@ export class WorkspaceComponent implements OnInit {
     this.labelschemes = this.workspace.labelschemes;
     this.labelschemes.push(this.workspace.EMPTY_SCHEME('None'))
     // default dataset/labelscheme
-    this.selected.dataset = this.datasets[0];
-    this.selected.labelscheme = this.labelschemes[0];
+    this.selected.dataset = this.defaultDataset();
+    this.selected.labelscheme = this.defaultScheme();
     // set dropdown classes
     this.classes = 'drop-down-selection w-100';
     this.dsMenu.classList = this.classes;
@@ -49,5 +49,72 @@ export class WorkspaceComponent implements OnInit {
   selectData(dataset: DataInfo) { this.selected.dataset = dataset; }
 
   selectLabels(labelscheme: LabelScheme) { this.selected.labelscheme = labelscheme }
+  // #endregion
+
+  // #region [Accessors]
+  get flashcount() {
+    let count = this.selected.dataset.flashes.length;
+    if (count === 0) return false;
+    return count;
+  }
+
+  get typescount() { return Object.keys(this.selected.labelscheme.event_map).length }
+
+  get flashBadge() { 
+    return {'badge-danger': this.flashcount === false,
+            'badge-success': this.flashcount !== false} 
+  }
+
+  get formatBadge() {
+    return {'badge-primary': this.selected.dataset.format === 'csv',
+            'badge-secondary': this.selected.dataset.format !== 'csv'}
+  }
+
+  get hasLabelsBadge() {
+    return {'badge-success': this.selected.labelscheme.hasLabels,
+            'badge-danger': !this.selected.labelscheme.hasLabels}
+  }
+
+  get typesBadge() {
+    return {'badge-primary': this.typescount > 0,
+            'badge-secondary': this.typescount === 0}
+  }
+  // #endregion
+
+  // #region [Helper Methods]
+
+  private defaultDataset() {
+    let datasets = this.datasets.filter((ds) => ds.flashes.length > 0);
+    if (datasets.length === 0) return this.datasets[0];
+    datasets = this.orderByType(datasets);
+    return datasets[0];
+  }
+
+  private defaultScheme() {
+    let schemes = this.labelschemes.filter((ls) => ls.hasLabels);
+    if (schemes.length === 0) return this.labelschemes[0];
+    schemes = this.orderByVideo(schemes);
+    return schemes[0];
+  }
+
+  private orderByType(datasets) {
+    let result = []
+    result.push(...this.sortByChannels(datasets.filter((ds) => ds.format === 'csv')))
+    result.push(...this.sortByChannels(datasets.filter((ds) => ds.format === 'tensor')))
+    result.push(...this.sortByChannels(datasets.filter((ds) => ds.format === 'bdl')))
+    return result
+  }
+
+  private sortByChannels(datasets) {
+    let comparator = (a,b) => { return a.channels.length - b.channels.length }
+    return datasets.sort(comparator)
+  }
+
+  private orderByVideo(schemes) {
+    let result = []
+    result.push(...schemes.filter((ls) => "video" in ls ))
+    result.push(...schemes.filter((ls) => !("video" in ls) ))
+    return result
+  }
   // #endregion
 }
