c3b4f0b065085fe9c6e8d81b9856428977dcae53
Lawrence Thatcher <lwthatcher@msn.com>
2018-07-26 12:29:32 -0600

    cut out old home-page leftovers

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,25 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { trigger, style, animate, transition, query, stagger, keyframes } from '@angular/animations';
-import { WorkspaceInfo } from '../data-loader/workspace-info';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.component.html',
-  styleUrls: ['home.component.css'],
-  animations: [
-    trigger("workspace", [
-      transition(':enter', [
-        query(':enter', style({ opacity: 0 }), {optional: true}),
-        query(':enter', stagger('300ms', [
-          animate('1s ease-in', keyframes([
-            style({opacity: 0, transform: 'translateY(-75%)', offset: 0}),
-            style({opacity: .5, transform: 'translateY(35px)',  offset: 0.3}),
-            style({opacity: 1, transform: 'translateY(0)',     offset: 1.0}),
-          ]))]), {optional: true})
-      ])
-    ])
-  ]
+  styleUrls: ['home.component.css']
 })
 export class HomeComponent implements OnInit {
   // #region [Constructors]
