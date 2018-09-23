8d023fda21daf72fa0e07d442e723d8f94ec6a8c
Lawrence Thatcher <lwthatcher@msn.com>
2018-09-22 23:44:48 -0600

    updated unit tests

--- src/app/dataview/energy/lazy-map.spec.ts
+++ src/app/dataview/energy/lazy-map.spec.ts
@@ -1,32 +1,65 @@
 import { LazyMap } from './lazy-map';
 
+// #region [Constants]
+const COLORS = {
+    r: Promise.resolve('red'),
+    g: Promise.resolve('green'),
+    b: Promise.resolve('blue')
+}
+// #endregion
+
+// #region [Helper Classes]
 class MockMapHolder {
     firstmap;
     secondmap;
     private ds: Promise<any>;
-    constructor(ds) {
-        this.ds = ds;
+    private i = 0;
+    constructor(color) {
+        this.ds = Promise.resolve(color);
         this.firstmap = new LazyMap(() => this.first(this.ds));
         this.secondmap = new LazyMap(() => this.second(this.ds));
     }
-    update(ds) { this.ds = ds; }
+    update(color) { this.ds = Promise.resolve(color); }
     first(ds) { return ds.then(d => d[0]) }
     second(ds) { return ds.then(d => d[1]) }
+    increment(ds) { this.i += 1; return Promise.resolve({d: ds, i: this.i}) }
 }
+// #endregion
 
 describe('LazyMap', () => {
 
-    let A = Promise.resolve('blue');
-    let B = Promise.resolve('red');
-    let C = Promise.resolve([10, 20, 30, 40, 50]);
-    let D = Promise.resolve({red: "warm", blue: "cool", yellow: "gross"})
+    const COLORS = {
+        r: 'red',
+        g: 'green',
+        b: 'blue'
+    }
     
     beforeEach(() => {});
    
     it('get -- handles promises', () => {
-        const m = new MockMapHolder(A);
-        let a = m.firstmap.get('a');
-        a.then(answer => { expect(answer).toBe('b') })
+        let l = 'R';
+        const m = new MockMapHolder(COLORS[l]);
+        let [w1, w2] = [m.firstmap.get(l), m.secondmap.get(l)]
+        w1.then(answer => { expect(answer).toBe('r') })
+        w2.then(answer => { expect(answer).toBe('e') })
+    });
+
+    it('get -- callback pointing to changing object', () => {
+        let l = 'R';
+        const m = new MockMapHolder(COLORS[l]);
+        let [w1, w2] = [m.firstmap.get(l), m.secondmap.get(l)]
+        w1.then(answer => { expect(answer).toBe('r') })
+        w2.then(answer => { expect(answer).toBe('e') })
+        l = 'B';
+        m.update(COLORS[l]);
+        [w1, w2] = [m.firstmap.get(l), m.secondmap.get(l)]
+        w1.then(answer => { expect(answer).toBe('b') })
+        w2.then(answer => { expect(answer).toBe('l') })
+        l = 'R';
+        m.update(COLORS[l]);
+        [w1, w2] = [m.firstmap.get(l), m.secondmap.get(l)]
+        w1.then(answer => { expect(answer).toBe('r') })
+        w2.then(answer => { expect(answer).toBe('e') })
     });
 
 });
